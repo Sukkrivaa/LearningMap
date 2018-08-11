1a49a946af531784d7d352210d490f58345d2df1
Sukkrivaa <sukkrivaa.vijayan@gmail.com>
2018-08-12 02:45:25 +0800

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+	//Webapack Configuration
+	mode: "development",
+	entry: './app/index.js',
+	output: {
+		filename: "./public/bundle.js"
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.(jsx?)$/\,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: "style-loader"
+				},
+				{
+					loader: "css-loader",
+					options: {
+						modules: true,
+						camelCase: true,
+						sourceMap: true
+					}
+				},{
+					loader: "sass-loader"
+				}]
+			}
+		]
+	}
+};
