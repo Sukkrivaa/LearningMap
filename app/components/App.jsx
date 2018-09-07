2a2d0fc32019d6aeb33a3216c029c7aeea975ae9
Sukkrivaa <sukkrivaa.vijayan@gmail.com>
2018-09-07 20:16:53 +0800

    finished the ordering part of the subtopics

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -9,10 +9,10 @@ class App extends Component{
 	constructor(){
 		super();
 		this.onAddSubTopic = this.onAddSubTopic.bind(this);
-		this.orderSubtopics = this.orderSubtopics.bind(this);
+		// this.orderSubtopics = this.orderSubtopics.bind(this);
 		this.renderSubtopics = this.renderSubtopics.bind(this);
 		this.onChangeOrder = this.onChangeOrder.bind(this);
-		this.forceRerender = this.forceRerender.bind(this);
+		// this.forceRerender = this.forceRerender.bind(this);
 	}
 
 	onAddSubTopic(e){
@@ -23,24 +23,18 @@ class App extends Component{
 		this.props.dispatch(addSubtopic(subtopic, order, timeCreated));
 	}
 
-	orderSubtopics(suptopicsArray){
-		// Need to look at the second property of every index and order the Subtopics
-			var repeatedOrder = false;
-			return suptopicsArray.sort((a,b) => {
-				let orderVal = a.order-b.order
-				var timeVal = 0;
-				if(orderVal === 0){
-					repeatedOrder = true;
-					timeVal = a.unixUpdated - b.unixUpdated;
-				}
-				return orderVal === 0 ? timeVal : orderVal;
-			})
-
-	}
-
-	forceRerender(){
-		this.forceUpdate();
-	}
+	// orderSubtopics(suptopicsArray){
+	// 	// Need to look at the second property of every index and order the Subtopics
+	// 		return suptopicsArray.sort((a,b) => {
+	// 			let orderVal = a.order-b.order
+	// 			var timeVal = 0;
+	// 			if(orderVal === 0){
+	// 				timeVal = a.unixUpdated - b.unixUpdated;
+	// 			}
+	// 			return orderVal === 0 ? timeVal : orderVal;
+	// 		})
+	//
+	// }
 
 	//Function to be passed down
 	onChangeOrder(text){
@@ -50,13 +44,12 @@ class App extends Component{
 	    //action to change the order of the subtopics - use id once you connect to mongoDB
 	    this.props.dispatch(changeOrderSubtopic(text, newOrder))
 		}
-		this.forceRerender();
 	}
 
 	renderSubtopics(){
 		var subtopics = this.props.subtopics
 		//Ordering the Subtopics once the order is set
-		subtopics = this.orderSubtopics(subtopics); //actually orders the state for you!! - does it by reference
+		// subtopics = this.orderSubtopics(subtopics); //actually orders the state for you!! - does it by reference
 		//Return the JSX
 		return subtopics.map((subtopic, index) => {
 			return (
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -4,7 +4,20 @@ export var updateSubtopicReducer = (state = [], action) =>{
 	switch (action.type){
 		case "ADD_SUBTOPIC":
 			//Add the subtopic to the current list of subtopic using es7 destructuring
-			return [...state, {subtopic: action.subtopic, order: action.order, unixUpdated: action.timeCreated}];
+			var arrayOfSubtopics = [...state, {subtopic: action.subtopic, order: parseInt(action.order), unixUpdated: action.timeCreated}];
+			arrayOfSubtopics.sort((a,b) => {
+				var orderVal = a.order - b.order;
+				var timeVal = 0;
+				if(orderVal === 0){
+					timeVal = b.unixUpdated - a.unixUpdated //negative means that a was created before b and thus should be put before
+				}
+				return orderVal === 0 ? timeVal : orderVal;
+			})
+
+			for(var i = 0; i <arrayOfSubtopics.length; i++){
+				arrayOfSubtopics[i].order = (i+1)
+			}
+			return arrayOfSubtopics;
 		case "NORMALIZE_ORDER_SUBTOPIC":
 			// var repeatedOrder = false;
 			// //Sorting the state according to order
@@ -17,19 +30,39 @@ export var updateSubtopicReducer = (state = [], action) =>{
 			// 	}
 			// 	return orderVal === 0 ? timeVal : orderVal;
 			// })
-			var newState = state.slice();
-			for(var i = 0; i <newState.length; i++){
-				newState[i].order = (i+1)
-			}
-			return newState;
+			// var newState = state.slice();
+			// for(var i = 0; i <newState.length; i++){
+			// 	newState[i].order = (i+1)
+			// }
+			// return newState;
 		case "CHANGE_ORDER_SUBTOPIC":
 			// We were mutating here - need to prevent that by making a copy
 			var newState = [...state];
 			var index = newState.findIndex(subtopic => subtopic.subtopic === action.subtopic);
-			console.log("index: " + index)
-			console.log(newState[index])
-			newState[index]["order"] = parseInt(action.newOrder);
+			// console.log("index: " + index)
+			// console.log(newState[index])
+			var oldOrder = newState[index]["order"];
+			if(oldOrder < action.newOrder){
+				newState[index]["order"] = parseInt(action.newOrder) + 1;
+			}else{
+				newState[index]["order"] = parseInt(action.newOrder);
+			}
 			newState[index]["unixUpdated"] = moment().unix();
+			// console.log("Currently" + newState[index].order)
+			//timeVal should be opposite if sorting up and normal when sorting down
+			console.log("Before Sorting ", newState);
+			newState.sort((a,b) => {
+				var orderVal = a.order - b.order;
+				var timeVal = 0;
+				if(orderVal === 0){
+					timeVal = b.unixUpdated - a.unixUpdated;
+				}
+				return orderVal === 0 ? timeVal : orderVal;
+			})
+			console.log("After Sorting ", newState);
+			for(var i = 0; i <newState.length; i++){
+				newState[i].order = (i+1)
+			}
 			return newState;
 		default:
 			return state;
